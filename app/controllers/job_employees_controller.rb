786c06d37889a7a563ae67bd01bb9f99a82f3479
Mariana Gogashvili <marianagogashvili24@gmail.com>
2019-05-12 22:12:12 +0300

    job view

--- app/controllers/job_employees_controller.rb
+++ app/controllers/job_employees_controller.rb
@@ -2,12 +2,16 @@ class JobEmployeesController < ApplicationController
   before_action :require_employer, only: [:disapprove, :approve]
   before_action :before_email, only: [:disapprove, :approve]
   def create
-    @job_employee = JobEmployee.new(job_id: params[:id], employee_id: session[:employee_id])
-    @job = Job.find(params[:id])
-    if check() == false
-      if @job.vacant = true
-        @job_employee.save
+    if session[:employee_id] != nil
+      @job_employee = JobEmployee.new(job_id: params[:id], employee_id: session[:employee_id])
+      @job = Job.find(params[:id])
+      if check() == false
+        if @job.vacant = true
+          @job_employee.save
+        end
       end
+    else
+      redirect_to jobs_path
     end
   end
 
@@ -40,8 +44,10 @@ class JobEmployeesController < ApplicationController
   def require_employer
     @je = JobEmployee.find_by(job_id: params[:job_id])
     @job = Job.find(params[:job_id])
-    if !logged_in? || session[:employer_id] != @job.employer.id
-      redirect_to jobs_path
+    if !logged_in?
+      if session[:employer_id] != @job.employer.id
+          redirect_to jobs_path
+      end
     end
   end
 
